3baeb5fbd8e1262919e0b9297e7d2546f8980e73
sainadh-paruchuri <saiparuchuri121@gmail.com>
2023-01-07 10:30:11 +0530

    final commit

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USER_NAME,process.env.DB_PASSWORD,{
     dialect:'mysql',
-    host:'localhost'
+    host:process.env.DB_HOST
 })
 
 module.exports=sequelize;
